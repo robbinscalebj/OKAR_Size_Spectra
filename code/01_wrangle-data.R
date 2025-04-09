d1ff4e789f6efe1d0fda07971e4ead222936edd7
Jim Junker <james.junker1@gmail.com>
2025-04-09 11:00:30 -0500

    wrangle code.

---
+++ code/01_wrangle-data.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+library(tidybayes)
+library(isdbayes)
+library(poweRlaw)
+library(readxl)
+library(here)
+here::i_am("code/01_wrangle-data.R")
+
+inverts_full = read_excel(
+  path = here("data/20160906_Analysis_SRJS_Macroinvertebrates.xlsx"),
+  sheet = "Site Data",
+  range = cell_cols("A:AW")
+  )
+
+inverts_int = inverts_full %>% 
+  select(site, event, month, year, sort, taxon_lifestage,
+         code, count, matches("l\\d{1}"),matches("h\\d{1}"), length_a, length_b, head_a, head_b) %>% 
+  rowwise %>% 
+  mutate(across(where(matches("l\\d{1}")), ~length_a*(.x^length_b))) %>% 
+  mutate(across(where(matches("h\\d{1}")), ))
+  
+  group_by(site, event, month, year, sort, taxon_lifestage, code, count, length_a, length_b, head_a, head_b) %>% 
+  pivot_longer()
